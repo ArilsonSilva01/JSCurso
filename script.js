e724da2a1df7c3de22609beee288b0bebdb42357
Alberto <betim009@gmail.com>
2024-05-08 19:56:30 -0300

    iniciando estrutura de dados + resumo

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log("teste")
+// Simbolo             Descrição
+// ()                  Função/Método
+// {}                  Objeto/Escopo
+// []                  Array
+// .                   Acessando um objeto/Acessando um método
+// ""                  string
+
+// Saída de dados
+console.log()
+
+
+const remedio1 = "Amoxilina";
+// Tipo da variavel: const    -> NÃO ALTERA VALOR
+// Nome da variavel: remedio1
+// Valor da Variavel: "Amoxilina"
+// Tipo do dado: string
+
+let remedio2 = "Dipirona";   
+// Tipo da variavel: let    -> ALTERA VALOR
+// Nome da variavel: remedio2
+// Valor da Variavel: "Dipirona"
+// Tipo do dado: string
+
+
+if (condition) {
+    // Aqui escreve o escopo do código
+};
+
+for (condition; condition;) {
+    // Aqui escreve o escopo do código
+};
+
+// Função Tradicional
+function name(params) {
+    // Aqui escreve o escopo do código
+};
+
+// Funçaõ Arrow Function
+const name1 = () => {
+
+};
+
+while (condition) {
+    
+};
---
+++ script1.js
@@ -0,0 +1,33 @@
+// Começar a construir uma estrutura de DADOS
+const usuarios = [
+    {
+        id: 1,
+        nome: "João",
+        email: "joao@email.com",
+        password: "joao123",
+        telefone: "(92)99999999",
+    }
+];
+
+const remedios = [
+    {
+        id: 1,
+        idUsuario: 1,
+        nome: "Cataflam",
+        detalhes: "Caixa com 24 comprimidos",
+        validade: "05-20-2025",
+        unidade: 3,
+        quantidade: 2,
+        controlado: false,
+    },
+    {
+        id: 2,
+        idUsuario: 1,
+        nome: "Lorsatana",
+        detalhes: "Cartela com 6/12 comprimidos",
+        validade: "01-05-2027",
+        unidade: 6,
+        quantidade: 1,
+        controlado: false,
+    }
+];
